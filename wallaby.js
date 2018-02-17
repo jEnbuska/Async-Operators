54e14dfae488c6f9bef9f66134492d9665f24a37
jenbuska <joona.enbuska@eficode.com>
2018-02-17 10:55:10 +0200

    create wallaby config

---
+++ wallaby.js
@@ -0,0 +1,29 @@
+module.exports = function (wallaby) {
+    return {
+        files: [
+            'src/**/*.js',
+            'index.js',
+            'tests/common.js',
+            'package.json',
+        ],
+
+        tests: [
+            'tests/**/*test.js',
+        ],
+        env: {
+            type: 'node',
+            runner: 'node',
+        },
+
+        setup () {
+            process.env.NODE_ENV = 'test';
+        },
+
+        testFramework: 'jest',
+        compilers: {
+            'src/**/*.js': wallaby.compilers.babel(),
+            'index.js': wallaby.compilers.babel(),
+            'tests/**/*.js': wallaby.compilers.babel(),
+        },
+    };
+};
