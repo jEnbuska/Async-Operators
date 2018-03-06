df1185b042beb9cb7675b5d1eba80f54b4a63779
jenbuska <joonaenb@gmail.com>
2018-03-06 20:52:38 +0200

    add callback provider test

---
+++ tests/operators/callback.test.js
@@ -0,0 +1,26 @@
+import { provider, } from '../../';
+import { sleep, sleepAndReturn, } from '../common';
+
+describe('provider from callback', () => {
+
+    test('callback', async() => {
+
+        async function oneTwoThree (onNext) {
+            await sleep(10);
+            onNext(1);
+            await sleep(10);
+            onNext(2);
+            await sleep(10);
+            onNext(3);
+        }
+
+        const results = await provider({
+            async callback ({ onNext, onComplete, }) {
+                await oneTwoThree(onNext);
+                onComplete();
+            }, })
+            .reduce((acc, int) => [ ...acc, int, ], [])
+            .pull();
+        expect(results).toEqual([ 1, 2, 3, ]);
+    });
+});
